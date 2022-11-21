153040cd8160fac04c318b5e07167c461062d543
Hamada Aljarrah <hamadasepost@gmail.com>
2022-11-21 15:49:58 +0100

    test

--- index.ts
+++ index.ts
@@ -7,14 +7,28 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get("/", (req:Request, res:Response) => { res.json({ message: "Hello from the server" }) });
+const USERS = [
+    {
+        name: "Hamada",
+        email: "hamada@gmail.com"
+    },
+    {
+        name: "Stina",
+        email: "stina@gmail.com"
+    },
+    {
+        name: "Taqaung",
+        email: "Taqaung@gmail.com"
+    },
+]
+app.get("/users", (req: Request, res: Response) => res.json({ data: USERS }))
 
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log("listning to port* 4000"));
-
-
 /*
 git add .
 git commit -am "test"
 git push heroku main
+
 */
